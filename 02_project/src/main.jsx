9fba8d7ef05251c9fbf552e63404426f1d152d3d
Rishav <rishavofficial0709@gmail.com>
2026-01-16 11:51:52 +0000

    .

--- 02_project/src/main.jsx
+++ 02_project/src/main.jsx
@@ -1,8 +1,38 @@
-
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+    return (
+        <div>
+            <h1> Custom App !! </h1>
+        </div>
+    )
+}
+// const ReactElement = {
+//     type: 'a',
+//     props: {
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'click me to visit google'
+// }
+
+const anotherElement = (
+    <a href="https://google.com" target = '_blank'> Visit google </a>
+)
+
+const anotherUser = " with Rishav"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+    anotherElement
+)
+
 createRoot(document.getElementById('root')).render(
-    <App />
+
+    reactElement 
 
 )
